47a059b32b2467ddf2a952300e67fb487dd2a41f
Cristian Ramirez <cristiano.rosetti@gmail.com>
2025-05-02 12:09:09 -0600

    Update main - variables.tf

--- provision/main - variables.tf
+++ provision/main - variables.tf
@@ -30,7 +30,7 @@ variable "gke_version_prefix" {
 }
 variable "gke_num_nodes" {
   description = "number of gke nodes"
-  default     = 1
+  default     = 2
 }
 variable "tags" {
   default = ["gke-node", "sales-209522-gke"]
